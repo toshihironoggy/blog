838d9e081c9de7de7683d130da52879eabdba66e
ToshiNogi <toshihironogi8@gmail.com>
2022-04-21 22:03:23 +0900

    add code

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
